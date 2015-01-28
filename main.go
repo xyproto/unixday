076fcb2618d556e735792d8f08c239df353c8bf9
Alexander F Rødseth <rodseth@gmail.com>
2015-01-28 08:59:44 +0100

    Added comments about leap seconds

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	// UNIX time does not take leap seconds into account. Neither does Unix Days.
 	secondsaday = 3600 * 24
 )
 
@@ -14,6 +15,7 @@ func main() {
 	dateFlag := flag.Int("date", 0, "A date, given as unix days")
 	flag.Parse()
 	if *dateFlag != 0 {
+		// Be aware that UNIX days and normal days does not match, because of leap seconds.
 		fmt.Println(time.Unix(int64(*dateFlag)*secondsaday, 0).UTC().Format("02.01.2006"))
 		return
 	}
